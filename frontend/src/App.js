9252858ae1e611ede691eb147c48790d309caa2c
Vaishnavi-Patil23 <vp657741@gmail.com>
2025-01-23 15:17:15 +0530

    added signup popup

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,34 @@
-import SignupRector from './Components/SignupRector.js';
-import './App.css';
+import React, { useState } from "react";
+import SignupRector from "./Components/SignupRector/SignupRector.js";
+import SignupGuard from "./Components/SignupGuard/SignupGuard.js";
+import SignupOptions from "./Components/SignupOptions/SignupOptions.js";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import "./App.css";
 
 function App() {
+  const [showSignupOptions, setShowSignupOptions] = useState(false);
+
   return (
-    <div>
-      <SignupRector/>
-    </div>
+    <Router>
+      <div className="app">
+        <button
+          className="open-signup-btn"
+          onClick={() => setShowSignupOptions(true)}
+        >
+          Signup
+        </button>
+
+        {showSignupOptions && (
+          <SignupOptions onClose={() => setShowSignupOptions(false)} />
+        )}
+
+        <Routes>
+          {/* <Route path="/SignupStudent" element={<SignupStudent />} /> */}
+          <Route path="/SignupRector" element={<SignupRector />} />
+          <Route path="/SignupGuard" element={<SignupGuard />} />
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
--- frontend/src/Components/SignupRector/SignupRector.js
+++ frontend/src/Components/SignupRector/SignupRector.js
@@ -6,6 +6,7 @@ const SignupRector = () => {
     name: "",
     email: "",
     mobile: "",
+    Address: "",
     position: "",
     idProof: null,
     password: "",
@@ -68,6 +69,18 @@ const SignupRector = () => {
               id="mobile"
               name="mobile"
               value={formData.mobile}
+              onChange={handleChange}
+              required
+            />
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="address">Address</label>
+            <input
+              type="text"
+              id="address"
+              name="address"
+              value={formData.address}
               onChange={handleChange}
               required
             />
