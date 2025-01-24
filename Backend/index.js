d05368f05f8c3fde29f4686d5299a620045dc51b
Vaishnavi-Patil23 <vp657741@gmail.com>
2025-01-24 15:53:02 +0530

    gitignore edited

--- Backend/index.js
+++ Backend/index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const connectDb = require('./utils/db');
 const cors = require('cors');
 const express = require("express");
-const authRoutes = require("./routes/auth");
+const rectorRoutes = require("./routes/rectorRoutes");
 const path = require('path');
 
 const app = express(); 
@@ -13,7 +13,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
-app.use("/auth", authRoutes);
+app.use("/auth", rectorRoutes);
 
 connectDb().then(() => {
     app.listen(PORT, () => {
